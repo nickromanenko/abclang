c30a65cb1ae10cd25451b517df4649c378be2853
Nick <wezawe@gmail.com>
2025-02-13 23:00:54 +0100

    Process events

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,15 @@ import compression from 'compression';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 import moment from 'moment';
+import {
+    processExistingBooking,
+    processGeneralEnquiry,
+    processMarketing,
+    processNewBooking,
+    processRecruitment,
+    processSupplierAccounts,
+    processVisitingOffice,
+} from './services/bot.service';
 
 const app = express();
 const router = express.Router();
@@ -16,7 +25,6 @@ router.use(
 router.use(express.urlencoded({ extended: true, limit: '150mb' }));
 
 router.get('/', (req: Request, res: Response) => res.json({ message: 'ABCLANG API v1.01' }));
-
 router.get('/start', (req: Request, res: Response) => {
     console.log('[🚀] START');
     return res.json({ current_time: moment().format('MMMM Do YYYY, dddd, HH:mm') });
@@ -27,7 +35,33 @@ router.post('/start', (req: Request, res: Response) => {
 });
 router.post('/event', async (req: Request, res: Response) => {
     console.log('[🎭] Event:', req.body.event);
+    if (req.body.event === 'call_analyzed') {
+        const call = req.body.call;
+        console.log('[📞] Call:', JSON.stringify(call));
+        const category = call?.call_analysis?.custom_analysis_data?.category || '';
+        console.log('[🗂️] Category:', category);
+        if (category === 'new_booking') {
+            await processNewBooking(call.transcript);
+        } else if (category === 'existing_project') {
+            await processExistingBooking(call.transcript);
+        } else if (category === 'general_enquiry') {
+            await processGeneralEnquiry(call.transcript);
+        } else if (category === 'supplier_accounts') {
+            await processSupplierAccounts(call.transcript);
+        } else if (category === 'recruitment') {
+            await processRecruitment(call.transcript);
+        } else if (category === 'visiting_office') {
+            await processVisitingOffice(call.transcript);
+        } else if (category === 'marketing') {
+            await processMarketing(call.transcript);
+        }
 
+        try {
+            // Update Report
+        } catch (error) {
+            console.error('ERROR:', error);
+        }
+    }
     return res.json({ success: true });
 });
 
